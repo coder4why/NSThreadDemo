e62bb043e553f8902d1fba4f37aa8153ee26735f
hupingkang <13456864167@163.com>
2018-04-22 10:43:39 +0800

    Initial Commit

---
+++ GCDTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDTest
+//
+//  Created by hu ping kang on 2018/4/22.
+//  Copyright © 2018年 hu ping kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDTest
+//
+//  Created by hu ping kang on 2018/4/22.
+//  Copyright © 2018年 hu ping kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
